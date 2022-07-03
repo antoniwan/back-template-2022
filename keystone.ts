6e1615f79515df860e3d3bcdc4cbbe103abcc85b
Antonio Rodriguez <antoniwan@gmail.com>
2022-07-03 11:09:48 -0400

    Enable CORS for our backend server

--- keystone.ts
+++ keystone.ts
@@ -3,19 +3,21 @@ import 'dotenv/config'
 import { config } from '@keystone-6/core';
 import { lists } from './schema';
 import { withAuth, session } from './auth';
-
-const { APP_PORT, DB_PROVIDER, DB_URL } = process.env;
+const { APP_PORT, FRONTEND_APP_URL, DB_PROVIDER, DB_URL } = process.env;
 
 export default withAuth(
   config({
     server: {
-      port: APP_PORT || 3001
+      port: APP_PORT || 3001,
+      cors: {
+        origin: [FRONTEND_APP_URL],
+        credentials: true
+      }
     },
     db: {
       provider: DB_PROVIDER || "sqlite",
       url: DB_URL || "file:./keystone.db",
     },
-    // Admin UI https://keystonejs.com/docs/apis/config#ui
     ui: {
       isAccessAllowed: (context) => !!context.session?.data,
     },
