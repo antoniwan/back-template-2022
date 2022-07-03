07f2fcc286ec6c24efda0336e66b1878a50ac79e
Antonio Rodriguez <antoniwan@gmail.com>
2022-07-03 12:11:48 -0400

    Upgrades to the user schema

--- schemas/User.ts
+++ schemas/User.ts
@@ -2,7 +2,9 @@ import { list } from '@keystone-6/core';
 import {
   text,
   relationship,
-  password
+  password,
+  checkbox,
+  timestamp
 } from '@keystone-6/core/fields';
 
 export const User = list({
@@ -14,7 +16,23 @@ export const User = list({
       isFilterable: true,
     }),
     password: password({ validation: { isRequired: true } }),
+    isAdmin: checkbox({ defaultValue: false }),
     posts: relationship({ ref: 'Post.author', many: true }),
+    lastOnlineOn: timestamp ({
+      defaultValue: undefined,
+      db: {
+        isNullable: true,
+      }
+    }),
+    createdOn: timestamp({
+      defaultValue: {
+        "kind": "now"
+      },
+      validation: {
+        isRequired: true
+      },
+      isIndexed: true
+    }),
   },
   ui: {
     listView: {
